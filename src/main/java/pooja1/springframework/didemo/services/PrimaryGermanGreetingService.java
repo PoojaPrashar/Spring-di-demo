074737e8c9945031bd9d1c34600b7a3ac97e34ca
Pooja Prashar <PoojaPrashar@users.noreply.github.com>
2019-09-16 12:57:17 -0700

    Adding the default keyword

--- src/main/java/pooja1/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/pooja1/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -1,17 +1,18 @@
 package pooja1.springframework.didemo.services;
 
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService{
-
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Primary German Greeting";
     }
+
 }
